02900c21eb02e683d885459f4a02076c7034ee97
mandyisabel <158763813+mandyisabel@users.noreply.github.com>
2024-02-03 18:45:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "LETS GOOO see you on the 14th Jannis-baby😘😘";
+    yesMessage.textContent = "LETS GOOO see you on the 14th Jannis-Pupsi 😘😘";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
